74982b25cf94ea819079eec0b833282604a00baa
Федяй Виталий <vfedyay@hold.bayadera.ua>
2019-10-18 10:44:51 +0300

    update without continue
    Add checking arrays

--- Task 1/main.js
+++ Task 1/main.js
@@ -1,8 +1,17 @@
+function check(arr) {
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    if (!Number.isNaN(arr[i])) {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr
+}
+
 function sum(arr) {
   let sum = 0
   for (let i = 0; i < arr.length; i++) {
-    if (Number.isNaN(arr[i])) { continue }
-    else if (typeof arr[i] === 'number') { sum += arr[i] }
+    if (typeof arr[i] === 'number' && !isNaN(arr[i])) { sum += arr[i] }
   }
   return sum
 }
@@ -10,8 +19,7 @@ function sum(arr) {
 function min(arr) {
   let min = arr[0]
   for (let i = 0; i < arr.length; i++) {
-    if (Number.isNaN(arr[i])) { continue }
-    else if (typeof arr[i] === 'number' && arr[i] < min) { min = arr[i] }
+    if (typeof arr[i] === 'number' && !isNaN(arr[i]) && arr[i] < min) { min = arr[i] }
   }
   return min
 }
@@ -19,8 +27,11 @@ function min(arr) {
 function max(arr) {
   let max = arr[0]
   for (let i = 0; i < arr.length; i++) {
-    if (Number.isNaN(arr[i])) { continue }
-    else if (typeof arr[i] === 'number' && arr[i] > max) { max = arr[i] }
+    if (typeof arr[i] === 'number' && !isNaN(arr[i]) && arr[i] > max) { max = arr[i] }
   }
   return max
 }
+
+console.log('min: ' + min(check([NaN, 1, undefined, -12, true, 'qwerty', 10, NaN])))
+console.log('max: ' + max(check([NaN, 1, undefined, -12, true, 'qwerty', 10, NaN])))
+console.log('sum: ' + sum(check([NaN, 1, undefined, -12, true, 'qwerty', 10, NaN])))
